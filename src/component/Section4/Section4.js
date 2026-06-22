33ddffc21df075887b1161e566a5b5a391efac16
AlexRakotoarisoa <rakotoarisoaalex27@gmail.com>
2026-06-22 22:58:16 +0300

    Update sections

--- src/component/Section4/Section4.js
+++ src/component/Section4/Section4.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { 
-  FaFigma, FaJava, FaJs, FaPython, FaPhp, FaReact, FaShieldAlt, 
+import {  FaJava, FaJs, FaPython, FaPhp, FaReact, FaShieldAlt, 
   FaServer, FaNetworkWired, FaKey, FaNodeJs, FaDocker, FaGitAlt 
 } from "react-icons/fa";
 import { PiFileCSharp } from "react-icons/pi";
 import { BiLogoTypescript, BiLogoPostgresql, BiNetworkChart } from "react-icons/bi";
 import { 
-  SiDjango, SiTailwindcss, SiMongodb, SiAdobephotoshop, 
-  SiAdobeillustrator, SiGnubash 
+  SiDjango, SiTailwindcss, SiMongodb, SiGnubash 
 } from "react-icons/si";
 import { FiKey } from "react-icons/fi";
 import { TbBrandMysql, TbBrandGithub } from "react-icons/tb";
@@ -61,14 +59,6 @@ const Section4 = () => {
         { icon: <TbBrandMysql />, label: 'MySQL' },
         { icon: <BiLogoPostgresql />, label: 'PostgreSQL' },
         { icon: <SiMongodb />, label: 'MongoDB' },
-      ]
-    },
-    {
-      title: t('section4.competence4'),
-      items: [
-        { icon: <SiAdobephotoshop />, label: 'Adobe Photoshop' },
-        { icon: <SiAdobeillustrator />, label: 'Adobe Illustrator' },
-        { icon: <FaFigma />, label: 'Figma' },
       ]
     },
     {
